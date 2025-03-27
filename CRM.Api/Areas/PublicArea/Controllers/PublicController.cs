8a6ee86ac3a8b0eecbab8fd1528d2debe146d093
reemtarek <reemtarekmatr97@gmail.com>
2025-03-27 20:18:45 +0200

    add api

---
+++ CRM.Api/Areas/PublicArea/Controllers/PublicController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace CRM.Api.Areas.PublicArea.Controllers
+{
+    [Area("PublicArea")]
+    [DisplayName("Public Controller")]
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
--- CRM.Api/Program.cs
+++ CRM.Api/Program.cs
@@ -19,5 +19,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+
 
 app.Run();
