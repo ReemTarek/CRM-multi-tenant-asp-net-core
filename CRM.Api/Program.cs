232ea89b00cc9a79da753db4eb682c79bef6ab29
reemtarek <reemtarekmatr97@gmail.com>
2025-03-27 20:22:43 +0200

    add swagger ui

--- CRM.Api/Program.cs
+++ CRM.Api/Program.cs
@@ -5,13 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "CRM Api", Version = "v1" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CRM Api v1"));
 }
 
 app.UseHttpsRedirection();
